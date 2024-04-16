f89fde6f7dcc907df8f893019908c64f4a73f815
AnnsAnn✨ <git@annsann.eu>
2024-04-16 23:31:44 +0200

    Docs: Adjust to repo renaming

--- docs/astro.config.mjs
+++ docs/astro.config.mjs
@@ -3,11 +3,11 @@ import starlight from '@astrojs/starlight';
 
 // https://astro.build/config
 export default defineConfig({
-	base: '/riot-po-2024',
+	base: '/teamagochi',
 	site: 'https://smartuni.github.io',
 	integrations: [
 		starlight({
-			title: 'UNNAMED RIOT PROJECT',
+			title: 'Teamagochi',
 			sidebar: [
 				{
 					label: 'Basics',
