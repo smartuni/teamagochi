ebdb38fb3d75eeb5e8951c95c439b00c20304539
AnnsAnn✨ <git@annsann.eu>
2024-04-22 17:46:26 +0200

    Node: TFT Setup  (#37)
    Co-authored-by: moritzholzer <moritz.holzer@outlook.de>
    Co-authored-by: Moritz <moritz.holzer@haw-hamburg.de>

---
+++ node/src/bin/feather-nrf52840-sense/usbus_cdc_acm/cdc_acm.d
@@ -0,0 +1,202 @@
+/data/riotbuild/riotproject/node/src/bin/feather-nrf52840-sense/usbus_cdc_acm/cdc_acm.o: \
+ /data/riotbuild/riotbase/sys/usb/usbus/cdc/acm/cdc_acm.c \
+ /data/riotbuild/riotproject/node/src/bin/feather-nrf52840-sense/riotbuild/riotbuild.h \
+ /data/riotbuild/riotbase/core/lib/include/assert.h \
+ /opt/gcc-arm-none-eabi-10.3-2021.10/lib/gcc/arm-none-eabi/10.3.1/include/stdint.h \
+ /opt/gcc-arm-none-eabi-10.3-2021.10/arm-none-eabi/include/stdint.h \
+ /opt/gcc-arm-none-eabi-10.3-2021.10/arm-none-eabi/include/machine/_default_types.h \
+ /opt/gcc-arm-none-eabi-10.3-2021.10/arm-none-eabi/include/sys/features.h \
+ /opt/gcc-arm-none-eabi-10.3-2021.10/arm-none-eabi/include/_newlib_version.h \
+ /opt/gcc-arm-none-eabi-10.3-2021.10/arm-none-eabi/include/sys/_intsup.h \
+ /opt/gcc-arm-none-eabi-10.3-2021.10/arm-none-eabi/include/sys/_stdint.h \
+ /opt/gcc-arm-none-eabi-10.3-2021.10/arm-none-eabi/include/string.h \
+ /opt/gcc-arm-none-eabi-10.3-2021.10/arm-none-eabi/include/_ansi.h \
+ /opt/gcc-arm-none-eabi-10.3-2021.10/arm-none-eabi/include/newlib-nano/newlib.h \
+ /opt/gcc-arm-none-eabi-10.3-2021.10/arm-none-eabi/include/sys/config.h \
+ /opt/gcc-arm-none-eabi-10.3-2021.10/arm-none-eabi/include/machine/ieeefp.h \
+ /opt/gcc-arm-none-eabi-10.3-2021.10/arm-none-eabi/include/sys/reent.h \
+ /opt/gcc-arm-none-eabi-10.3-2021.10/arm-none-eabi/include/_ansi.h \
+ /opt/gcc-arm-none-eabi-10.3-2021.10/lib/gcc/arm-none-eabi/10.3.1/include/stddef.h \
+ /opt/gcc-arm-none-eabi-10.3-2021.10/arm-none-eabi/include/sys/_types.h \
+ /opt/gcc-arm-none-eabi-10.3-2021.10/arm-none-eabi/include/machine/_types.h \
+ /opt/gcc-arm-none-eabi-10.3-2021.10/arm-none-eabi/include/sys/lock.h \
+ /opt/gcc-arm-none-eabi-10.3-2021.10/arm-none-eabi/include/sys/cdefs.h \
+ /opt/gcc-arm-none-eabi-10.3-2021.10/arm-none-eabi/include/sys/string.h \
+ /data/riotbuild/riotbase/sys/include/tsrb.h \
+ /data/riotbuild/riotbase/core/lib/include/irq.h \
+ /opt/gcc-arm-none-eabi-10.3-2021.10/lib/gcc/arm-none-eabi/10.3.1/include/stdbool.h \
+ /data/riotbuild/riotbase/cpu/nrf52/include/cpu_conf.h \
+ /data/riotbuild/riotbase/cpu/cortexm_common/include/cpu_conf_common.h \
+ /data/riotbuild/riotbase/cpu/nrf52/include/vendor/nrf52840.h \
+ /data/riotbuild/riotbase/build/pkg/cmsis/CMSIS/Core/Include/core_cm4.h \
+ /data/riotbuild/riotbase/build/pkg/cmsis/CMSIS/Core/Include/cmsis_version.h \
+ /data/riotbuild/riotbase/build/pkg/cmsis/CMSIS/Core/Include/cmsis_compiler.h \
+ /data/riotbuild/riotbase/build/pkg/cmsis/CMSIS/Core/Include/cmsis_gcc.h \
+ /data/riotbuild/riotbase/build/pkg/cmsis/CMSIS/Core/Include/mpu_armv7.h \
+ /data/riotbuild/riotbase/cpu/nrf52/include/vendor/nrf52840_bitfields.h \
+ /data/riotbuild/riotbase/cpu/nrf52/include/vendor/nrf52840_peripherals.h \
+ /data/riotbuild/riotbase/cpu/cortexm_common/include/irq_arch.h \
+ /data/riotbuild/riotbase/core/lib/include/kernel_defines.h \
+ /data/riotbuild/riotbase/core/lib/include/modules.h \
+ /data/riotbuild/riotbase/core/lib/include/riot_version.h \
+ /data/riotbuild/riotbase/core/lib/include/compiler_hints.h \
+ /data/riotbuild/riotbase/core/lib/include/container.h \
+ /data/riotbuild/riotbase/sys/include/debug_irq_disable.h \
+ /data/riotbuild/riotbase/sys/include/usb/descriptor.h \
+ /data/riotbuild/riotbase/sys/include/usb/cdc.h \
+ /data/riotbuild/riotbase/sys/include/usb.h \
+ /data/riotbuild/riotbase/boards/feather-nrf52840-sense/include/board.h \
+ /data/riotbuild/riotbase/cpu/cortexm_common/include/cpu.h \
+ /opt/gcc-arm-none-eabi-10.3-2021.10/arm-none-eabi/include/stdio.h \
+ /opt/gcc-arm-none-eabi-10.3-2021.10/lib/gcc/arm-none-eabi/10.3.1/include/stdarg.h \
+ /opt/gcc-arm-none-eabi-10.3-2021.10/arm-none-eabi/include/sys/types.h \
+ /opt/gcc-arm-none-eabi-10.3-2021.10/arm-none-eabi/include/sys/_pthreadtypes.h \
+ /opt/gcc-arm-none-eabi-10.3-2021.10/arm-none-eabi/include/machine/types.h \
+ /opt/gcc-arm-none-eabi-10.3-2021.10/arm-none-eabi/include/sys/stdio.h \
+ /data/riotbuild/riotbase/core/include/sched.h \
+ /opt/gcc-arm-none-eabi-10.3-2021.10/arm-none-eabi/include/inttypes.h \
+ /data/riotbuild/riotbase/core/include/native_sched.h \
+ /data/riotbuild/riotbase/core/lib/include/clist.h \
+ /data/riotbuild/riotbase/core/lib/include/list.h \
+ /data/riotbuild/riotbase/core/include/thread.h \
+ /data/riotbuild/riotbase/core/lib/include/cib.h \
+ /data/riotbuild/riotbase/core/lib/include/assert.h \
+ /data/riotbuild/riotbase/core/include/msg.h \
+ /data/riotbuild/riotbase/core/include/sched.h \
+ /data/riotbuild/riotbase/core/lib/include/thread_config.h \
+ /data/riotbuild/riotbase/core/include/thread_flags.h \
+ /data/riotbuild/riotbase/cpu/cortexm_common/include/thread_arch.h \
+ /data/riotbuild/riotbase/boards/common/nrf52/include/board_common.h \
+ /data/riotbuild/riotbase/drivers/include/periph/gpio.h \
+ /opt/gcc-arm-none-eabi-10.3-2021.10/lib/gcc/arm-none-eabi/10.3.1/include-fixed/limits.h \
+ /opt/gcc-arm-none-eabi-10.3-2021.10/lib/gcc/arm-none-eabi/10.3.1/include-fixed/syslimits.h \
+ /opt/gcc-arm-none-eabi-10.3-2021.10/arm-none-eabi/include/limits.h \
+ /opt/gcc-arm-none-eabi-10.3-2021.10/arm-none-eabi/include/sys/syslimits.h \
+ /data/riotbuild/riotbase/cpu/nrf52/include/periph_cpu.h \
+ /data/riotbuild/riotbase/cpu/nrf5x_common/include/periph_cpu_common.h \
+ /data/riotbuild/riotbase/boards/feather-nrf52840-sense/include/periph_conf.h \
+ /data/riotbuild/riotbase/boards/common/nrf52/include/cfg_clock_32_0.h \
+ /data/riotbuild/riotbase/boards/common/nrf52/include/cfg_rtt_default.h \
+ /data/riotbuild/riotbase/boards/common/nrf52/include/cfg_timer_default.h \
+ /data/riotbuild/riotbase/sys/include/usb/usbus.h \
+ /opt/gcc-arm-none-eabi-10.3-2021.10/arm-none-eabi/include/stdlib.h \
+ /opt/gcc-arm-none-eabi-10.3-2021.10/arm-none-eabi/include/machine/stdlib.h \
+ /data/riotbuild/riotbase/sys/include/event.h \
+ /data/riotbuild/riotbase/core/include/thread_flags.h \
+ /data/riotbuild/riotbase/sys/include/ptrtag.h \
+ /data/riotbuild/riotbase/sys/include/ztimer.h \
+ /data/riotbuild/riotbase/core/include/msg.h \
+ /data/riotbuild/riotbase/core/include/mutex.h \
+ /data/riotbuild/riotbase/sys/include/architecture.h \
+ /data/riotbuild/riotbase/cpu/cortexm_common/include/architecture_arch.h \
+ /data/riotbuild/riotbase/core/lib/include/list.h \
+ /data/riotbuild/riotbase/core/include/thread.h \
+ /data/riotbuild/riotbase/core/lib/include/rmutex.h \
+ /data/riotbuild/riotbase/core/lib/include/modules.h \
+ /data/riotbuild/riotbase/drivers/include/periph/usbdev.h \
+ /data/riotbuild/riotbase/sys/include/usb/usbopt.h \
+ /data/riotbuild/riotbase/sys/include/usb/usbus/cdc/acm.h \
+ /data/riotbuild/riotbase/sys/include/usb/usbus/control.h \
+ /data/riotbuild/riotbase/sys/include/usb_board_reset_internal.h \
+ /data/riotbuild/riotbase/sys/include/usb/usbus/cdc/acm.h \
+ /data/riotbuild/riotbase/core/lib/include/debug.h
+/data/riotbuild/riotproject/node/src/bin/feather-nrf52840-sense/riotbuild/riotbuild.h:
+/data/riotbuild/riotbase/core/lib/include/assert.h:
+/opt/gcc-arm-none-eabi-10.3-2021.10/lib/gcc/arm-none-eabi/10.3.1/include/stdint.h:
+/opt/gcc-arm-none-eabi-10.3-2021.10/arm-none-eabi/include/stdint.h:
+/opt/gcc-arm-none-eabi-10.3-2021.10/arm-none-eabi/include/machine/_default_types.h:
+/opt/gcc-arm-none-eabi-10.3-2021.10/arm-none-eabi/include/sys/features.h:
+/opt/gcc-arm-none-eabi-10.3-2021.10/arm-none-eabi/include/_newlib_version.h:
+/opt/gcc-arm-none-eabi-10.3-2021.10/arm-none-eabi/include/sys/_intsup.h:
+/opt/gcc-arm-none-eabi-10.3-2021.10/arm-none-eabi/include/sys/_stdint.h:
+/opt/gcc-arm-none-eabi-10.3-2021.10/arm-none-eabi/include/string.h:
+/opt/gcc-arm-none-eabi-10.3-2021.10/arm-none-eabi/include/_ansi.h:
+/opt/gcc-arm-none-eabi-10.3-2021.10/arm-none-eabi/include/newlib-nano/newlib.h:
+/opt/gcc-arm-none-eabi-10.3-2021.10/arm-none-eabi/include/sys/config.h:
+/opt/gcc-arm-none-eabi-10.3-2021.10/arm-none-eabi/include/machine/ieeefp.h:
+/opt/gcc-arm-none-eabi-10.3-2021.10/arm-none-eabi/include/sys/reent.h:
+/opt/gcc-arm-none-eabi-10.3-2021.10/arm-none-eabi/include/_ansi.h:
+/opt/gcc-arm-none-eabi-10.3-2021.10/lib/gcc/arm-none-eabi/10.3.1/include/stddef.h:
+/opt/gcc-arm-none-eabi-10.3-2021.10/arm-none-eabi/include/sys/_types.h:
+/opt/gcc-arm-none-eabi-10.3-2021.10/arm-none-eabi/include/machine/_types.h:
+/opt/gcc-arm-none-eabi-10.3-2021.10/arm-none-eabi/include/sys/lock.h:
+/opt/gcc-arm-none-eabi-10.3-2021.10/arm-none-eabi/include/sys/cdefs.h:
+/opt/gcc-arm-none-eabi-10.3-2021.10/arm-none-eabi/include/sys/string.h:
+/data/riotbuild/riotbase/sys/include/tsrb.h:
+/data/riotbuild/riotbase/core/lib/include/irq.h:
+/opt/gcc-arm-none-eabi-10.3-2021.10/lib/gcc/arm-none-eabi/10.3.1/include/stdbool.h:
+/data/riotbuild/riotbase/cpu/nrf52/include/cpu_conf.h:
+/data/riotbuild/riotbase/cpu/cortexm_common/include/cpu_conf_common.h:
+/data/riotbuild/riotbase/cpu/nrf52/include/vendor/nrf52840.h:
+/data/riotbuild/riotbase/build/pkg/cmsis/CMSIS/Core/Include/core_cm4.h:
+/data/riotbuild/riotbase/build/pkg/cmsis/CMSIS/Core/Include/cmsis_version.h:
+/data/riotbuild/riotbase/build/pkg/cmsis/CMSIS/Core/Include/cmsis_compiler.h:
+/data/riotbuild/riotbase/build/pkg/cmsis/CMSIS/Core/Include/cmsis_gcc.h:
+/data/riotbuild/riotbase/build/pkg/cmsis/CMSIS/Core/Include/mpu_armv7.h:
+/data/riotbuild/riotbase/cpu/nrf52/include/vendor/nrf52840_bitfields.h:
+/data/riotbuild/riotbase/cpu/nrf52/include/vendor/nrf52840_peripherals.h:
+/data/riotbuild/riotbase/cpu/cortexm_common/include/irq_arch.h:
+/data/riotbuild/riotbase/core/lib/include/kernel_defines.h:
+/data/riotbuild/riotbase/core/lib/include/modules.h:
+/data/riotbuild/riotbase/core/lib/include/riot_version.h:
+/data/riotbuild/riotbase/core/lib/include/compiler_hints.h:
+/data/riotbuild/riotbase/core/lib/include/container.h:
+/data/riotbuild/riotbase/sys/include/debug_irq_disable.h:
+/data/riotbuild/riotbase/sys/include/usb/descriptor.h:
+/data/riotbuild/riotbase/sys/include/usb/cdc.h:
+/data/riotbuild/riotbase/sys/include/usb.h:
+/data/riotbuild/riotbase/boards/feather-nrf52840-sense/include/board.h:
+/data/riotbuild/riotbase/cpu/cortexm_common/include/cpu.h:
+/opt/gcc-arm-none-eabi-10.3-2021.10/arm-none-eabi/include/stdio.h:
+/opt/gcc-arm-none-eabi-10.3-2021.10/lib/gcc/arm-none-eabi/10.3.1/include/stdarg.h:
+/opt/gcc-arm-none-eabi-10.3-2021.10/arm-none-eabi/include/sys/types.h:
+/opt/gcc-arm-none-eabi-10.3-2021.10/arm-none-eabi/include/sys/_pthreadtypes.h:
+/opt/gcc-arm-none-eabi-10.3-2021.10/arm-none-eabi/include/machine/types.h:
+/opt/gcc-arm-none-eabi-10.3-2021.10/arm-none-eabi/include/sys/stdio.h:
+/data/riotbuild/riotbase/core/include/sched.h:
+/opt/gcc-arm-none-eabi-10.3-2021.10/arm-none-eabi/include/inttypes.h:
+/data/riotbuild/riotbase/core/include/native_sched.h:
+/data/riotbuild/riotbase/core/lib/include/clist.h:
+/data/riotbuild/riotbase/core/lib/include/list.h:
+/data/riotbuild/riotbase/core/include/thread.h:
+/data/riotbuild/riotbase/core/lib/include/cib.h:
+/data/riotbuild/riotbase/core/lib/include/assert.h:
+/data/riotbuild/riotbase/core/include/msg.h:
+/data/riotbuild/riotbase/core/include/sched.h:
+/data/riotbuild/riotbase/core/lib/include/thread_config.h:
+/data/riotbuild/riotbase/core/include/thread_flags.h:
+/data/riotbuild/riotbase/cpu/cortexm_common/include/thread_arch.h:
+/data/riotbuild/riotbase/boards/common/nrf52/include/board_common.h:
+/data/riotbuild/riotbase/drivers/include/periph/gpio.h:
+/opt/gcc-arm-none-eabi-10.3-2021.10/lib/gcc/arm-none-eabi/10.3.1/include-fixed/limits.h:
+/opt/gcc-arm-none-eabi-10.3-2021.10/lib/gcc/arm-none-eabi/10.3.1/include-fixed/syslimits.h:
+/opt/gcc-arm-none-eabi-10.3-2021.10/arm-none-eabi/include/limits.h:
+/opt/gcc-arm-none-eabi-10.3-2021.10/arm-none-eabi/include/sys/syslimits.h:
+/data/riotbuild/riotbase/cpu/nrf52/include/periph_cpu.h:
+/data/riotbuild/riotbase/cpu/nrf5x_common/include/periph_cpu_common.h:
+/data/riotbuild/riotbase/boards/feather-nrf52840-sense/include/periph_conf.h:
+/data/riotbuild/riotbase/boards/common/nrf52/include/cfg_clock_32_0.h:
+/data/riotbuild/riotbase/boards/common/nrf52/include/cfg_rtt_default.h:
+/data/riotbuild/riotbase/boards/common/nrf52/include/cfg_timer_default.h:
+/data/riotbuild/riotbase/sys/include/usb/usbus.h:
+/opt/gcc-arm-none-eabi-10.3-2021.10/arm-none-eabi/include/stdlib.h:
+/opt/gcc-arm-none-eabi-10.3-2021.10/arm-none-eabi/include/machine/stdlib.h:
+/data/riotbuild/riotbase/sys/include/event.h:
+/data/riotbuild/riotbase/core/include/thread_flags.h:
+/data/riotbuild/riotbase/sys/include/ptrtag.h:
+/data/riotbuild/riotbase/sys/include/ztimer.h:
+/data/riotbuild/riotbase/core/include/msg.h:
+/data/riotbuild/riotbase/core/include/mutex.h:
+/data/riotbuild/riotbase/sys/include/architecture.h:
+/data/riotbuild/riotbase/cpu/cortexm_common/include/architecture_arch.h:
+/data/riotbuild/riotbase/core/lib/include/list.h:
+/data/riotbuild/riotbase/core/include/thread.h:
+/data/riotbuild/riotbase/core/lib/include/rmutex.h:
+/data/riotbuild/riotbase/core/lib/include/modules.h:
+/data/riotbuild/riotbase/drivers/include/periph/usbdev.h:
+/data/riotbuild/riotbase/sys/include/usb/usbopt.h:
+/data/riotbuild/riotbase/sys/include/usb/usbus/cdc/acm.h:
+/data/riotbuild/riotbase/sys/include/usb/usbus/control.h:
+/data/riotbuild/riotbase/sys/include/usb_board_reset_internal.h:
+/data/riotbuild/riotbase/sys/include/usb/usbus/cdc/acm.h:
+/data/riotbuild/riotbase/core/lib/include/debug.h:
