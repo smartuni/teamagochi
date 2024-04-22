3e0496f0e39035dc2ea539068ac23aaf6ccf056a
Moritz <moritz.holzer@haw-hamburg.de>
2024-04-22 10:10:01 +0200

    moved tft code to src folder, created params file

---
+++ node/src/bin/feather-nrf52840-sense/core_lib/atomic_sync.d
@@ -0,0 +1,90 @@
+/data/riotbuild/riotproject/node/src/bin/feather-nrf52840-sense/core_lib/atomic_sync.o: \
+ /data/riotbuild/riotbase/core/lib/atomic_sync.c \
+ /data/riotbuild/riotproject/node/src/bin/feather-nrf52840-sense/riotbuild/riotbuild.h \
+ /opt/gcc-arm-none-eabi-10.3-2021.10/lib/gcc/arm-none-eabi/10.3.1/include/stddef.h \
+ /opt/gcc-arm-none-eabi-10.3-2021.10/arm-none-eabi/include/stdlib.h \
+ /opt/gcc-arm-none-eabi-10.3-2021.10/arm-none-eabi/include/machine/ieeefp.h \
+ /opt/gcc-arm-none-eabi-10.3-2021.10/arm-none-eabi/include/_ansi.h \
+ /opt/gcc-arm-none-eabi-10.3-2021.10/arm-none-eabi/include/newlib-nano/newlib.h \
+ /opt/gcc-arm-none-eabi-10.3-2021.10/arm-none-eabi/include/_newlib_version.h \
+ /opt/gcc-arm-none-eabi-10.3-2021.10/arm-none-eabi/include/sys/config.h \
+ /opt/gcc-arm-none-eabi-10.3-2021.10/arm-none-eabi/include/sys/features.h \
+ /opt/gcc-arm-none-eabi-10.3-2021.10/arm-none-eabi/include/sys/reent.h \
+ /opt/gcc-arm-none-eabi-10.3-2021.10/arm-none-eabi/include/_ansi.h \
+ /opt/gcc-arm-none-eabi-10.3-2021.10/arm-none-eabi/include/sys/_types.h \
+ /opt/gcc-arm-none-eabi-10.3-2021.10/arm-none-eabi/include/machine/_types.h \
+ /opt/gcc-arm-none-eabi-10.3-2021.10/arm-none-eabi/include/machine/_default_types.h \
+ /opt/gcc-arm-none-eabi-10.3-2021.10/arm-none-eabi/include/sys/lock.h \
+ /data/riotbuild/riotbase/core/lib/include/assert.h \
+ /opt/gcc-arm-none-eabi-10.3-2021.10/lib/gcc/arm-none-eabi/10.3.1/include/stdint.h \
+ /opt/gcc-arm-none-eabi-10.3-2021.10/arm-none-eabi/include/stdint.h \
+ /opt/gcc-arm-none-eabi-10.3-2021.10/arm-none-eabi/include/sys/_intsup.h \
+ /opt/gcc-arm-none-eabi-10.3-2021.10/arm-none-eabi/include/sys/_stdint.h \
+ /opt/gcc-arm-none-eabi-10.3-2021.10/arm-none-eabi/include/sys/cdefs.h \
+ /opt/gcc-arm-none-eabi-10.3-2021.10/arm-none-eabi/include/machine/stdlib.h \
+ /opt/gcc-arm-none-eabi-10.3-2021.10/lib/gcc/arm-none-eabi/10.3.1/include/stdbool.h \
+ /opt/gcc-arm-none-eabi-10.3-2021.10/lib/gcc/arm-none-eabi/10.3.1/include/stdatomic.h \
+ /opt/gcc-arm-none-eabi-10.3-2021.10/arm-none-eabi/include/string.h \
+ /opt/gcc-arm-none-eabi-10.3-2021.10/arm-none-eabi/include/sys/string.h \
+ /data/riotbuild/riotbase/core/lib/include/irq.h \
+ /data/riotbuild/riotbase/cpu/nrf52/include/cpu_conf.h \
+ /data/riotbuild/riotbase/cpu/cortexm_common/include/cpu_conf_common.h \
+ /data/riotbuild/riotbase/cpu/nrf52/include/vendor/nrf52840.h \
+ /data/riotbuild/riotbase/build/pkg/cmsis/CMSIS/Core/Include/core_cm4.h \
+ /data/riotbuild/riotbase/build/pkg/cmsis/CMSIS/Core/Include/cmsis_version.h \
+ /data/riotbuild/riotbase/build/pkg/cmsis/CMSIS/Core/Include/cmsis_compiler.h \
+ /data/riotbuild/riotbase/build/pkg/cmsis/CMSIS/Core/Include/cmsis_gcc.h \
+ /data/riotbuild/riotbase/build/pkg/cmsis/CMSIS/Core/Include/mpu_armv7.h \
+ /data/riotbuild/riotbase/cpu/nrf52/include/vendor/nrf52840_bitfields.h \
+ /data/riotbuild/riotbase/cpu/nrf52/include/vendor/nrf52840_peripherals.h \
+ /data/riotbuild/riotbase/cpu/cortexm_common/include/irq_arch.h \
+ /data/riotbuild/riotbase/core/lib/include/kernel_defines.h \
+ /data/riotbuild/riotbase/core/lib/include/modules.h \
+ /data/riotbuild/riotbase/core/lib/include/riot_version.h \
+ /data/riotbuild/riotbase/core/lib/include/compiler_hints.h \
+ /data/riotbuild/riotbase/core/lib/include/container.h \
+ /data/riotbuild/riotbase/sys/include/debug_irq_disable.h
+/data/riotbuild/riotproject/node/src/bin/feather-nrf52840-sense/riotbuild/riotbuild.h:
+/opt/gcc-arm-none-eabi-10.3-2021.10/lib/gcc/arm-none-eabi/10.3.1/include/stddef.h:
+/opt/gcc-arm-none-eabi-10.3-2021.10/arm-none-eabi/include/stdlib.h:
+/opt/gcc-arm-none-eabi-10.3-2021.10/arm-none-eabi/include/machine/ieeefp.h:
+/opt/gcc-arm-none-eabi-10.3-2021.10/arm-none-eabi/include/_ansi.h:
+/opt/gcc-arm-none-eabi-10.3-2021.10/arm-none-eabi/include/newlib-nano/newlib.h:
+/opt/gcc-arm-none-eabi-10.3-2021.10/arm-none-eabi/include/_newlib_version.h:
+/opt/gcc-arm-none-eabi-10.3-2021.10/arm-none-eabi/include/sys/config.h:
+/opt/gcc-arm-none-eabi-10.3-2021.10/arm-none-eabi/include/sys/features.h:
+/opt/gcc-arm-none-eabi-10.3-2021.10/arm-none-eabi/include/sys/reent.h:
+/opt/gcc-arm-none-eabi-10.3-2021.10/arm-none-eabi/include/_ansi.h:
+/opt/gcc-arm-none-eabi-10.3-2021.10/arm-none-eabi/include/sys/_types.h:
+/opt/gcc-arm-none-eabi-10.3-2021.10/arm-none-eabi/include/machine/_types.h:
+/opt/gcc-arm-none-eabi-10.3-2021.10/arm-none-eabi/include/machine/_default_types.h:
+/opt/gcc-arm-none-eabi-10.3-2021.10/arm-none-eabi/include/sys/lock.h:
+/data/riotbuild/riotbase/core/lib/include/assert.h:
+/opt/gcc-arm-none-eabi-10.3-2021.10/lib/gcc/arm-none-eabi/10.3.1/include/stdint.h:
+/opt/gcc-arm-none-eabi-10.3-2021.10/arm-none-eabi/include/stdint.h:
+/opt/gcc-arm-none-eabi-10.3-2021.10/arm-none-eabi/include/sys/_intsup.h:
+/opt/gcc-arm-none-eabi-10.3-2021.10/arm-none-eabi/include/sys/_stdint.h:
+/opt/gcc-arm-none-eabi-10.3-2021.10/arm-none-eabi/include/sys/cdefs.h:
+/opt/gcc-arm-none-eabi-10.3-2021.10/arm-none-eabi/include/machine/stdlib.h:
+/opt/gcc-arm-none-eabi-10.3-2021.10/lib/gcc/arm-none-eabi/10.3.1/include/stdbool.h:
+/opt/gcc-arm-none-eabi-10.3-2021.10/lib/gcc/arm-none-eabi/10.3.1/include/stdatomic.h:
+/opt/gcc-arm-none-eabi-10.3-2021.10/arm-none-eabi/include/string.h:
+/opt/gcc-arm-none-eabi-10.3-2021.10/arm-none-eabi/include/sys/string.h:
+/data/riotbuild/riotbase/core/lib/include/irq.h:
+/data/riotbuild/riotbase/cpu/nrf52/include/cpu_conf.h:
+/data/riotbuild/riotbase/cpu/cortexm_common/include/cpu_conf_common.h:
+/data/riotbuild/riotbase/cpu/nrf52/include/vendor/nrf52840.h:
+/data/riotbuild/riotbase/build/pkg/cmsis/CMSIS/Core/Include/core_cm4.h:
+/data/riotbuild/riotbase/build/pkg/cmsis/CMSIS/Core/Include/cmsis_version.h:
+/data/riotbuild/riotbase/build/pkg/cmsis/CMSIS/Core/Include/cmsis_compiler.h:
+/data/riotbuild/riotbase/build/pkg/cmsis/CMSIS/Core/Include/cmsis_gcc.h:
+/data/riotbuild/riotbase/build/pkg/cmsis/CMSIS/Core/Include/mpu_armv7.h:
+/data/riotbuild/riotbase/cpu/nrf52/include/vendor/nrf52840_bitfields.h:
+/data/riotbuild/riotbase/cpu/nrf52/include/vendor/nrf52840_peripherals.h:
+/data/riotbuild/riotbase/cpu/cortexm_common/include/irq_arch.h:
+/data/riotbuild/riotbase/core/lib/include/kernel_defines.h:
+/data/riotbuild/riotbase/core/lib/include/modules.h:
+/data/riotbuild/riotbase/core/lib/include/riot_version.h:
+/data/riotbuild/riotbase/core/lib/include/compiler_hints.h:
+/data/riotbuild/riotbase/core/lib/include/container.h:
+/data/riotbuild/riotbase/sys/include/debug_irq_disable.h:
