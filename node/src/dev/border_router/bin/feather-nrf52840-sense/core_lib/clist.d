04407ee28565f4a386a9a0b4443ae37baba0b103
Moritz <moritz.holzer@haw-hamburg.de>
2024-04-23 12:47:39 +0200

    border router working, lw2m not yet

---
+++ node/src/dev/border_router/bin/feather-nrf52840-sense/core_lib/clist.d
@@ -0,0 +1,12 @@
+/data/riotbuild/riotproject/node/src/dev/border_router/bin/feather-nrf52840-sense/core_lib/clist.o: \
+ /data/riotbuild/riotbase/core/lib/clist.c \
+ /data/riotbuild/riotproject/node/src/dev/border_router/bin/feather-nrf52840-sense/riotbuild/riotbuild.h \
+ /data/riotbuild/riotbase/core/lib/include/clist.h \
+ /opt/gcc-arm-none-eabi-10.3-2021.10/lib/gcc/arm-none-eabi/10.3.1/include/stdbool.h \
+ /opt/gcc-arm-none-eabi-10.3-2021.10/lib/gcc/arm-none-eabi/10.3.1/include/stddef.h \
+ /data/riotbuild/riotbase/core/lib/include/list.h
+/data/riotbuild/riotproject/node/src/dev/border_router/bin/feather-nrf52840-sense/riotbuild/riotbuild.h:
+/data/riotbuild/riotbase/core/lib/include/clist.h:
+/opt/gcc-arm-none-eabi-10.3-2021.10/lib/gcc/arm-none-eabi/10.3.1/include/stdbool.h:
+/opt/gcc-arm-none-eabi-10.3-2021.10/lib/gcc/arm-none-eabi/10.3.1/include/stddef.h:
+/data/riotbuild/riotbase/core/lib/include/list.h:
---
+++ node/src/dev/lwm2m/bin/feather-nrf52840-sense/gnrc_sixlowpan_frag_fb/gnrc_sixlowpan_frag_fb.d
@@ -0,0 +1,96 @@
+/data/riotbuild/riotproject/node/src/dev/lwm2m/bin/feather-nrf52840-sense/gnrc_sixlowpan_frag_fb/gnrc_sixlowpan_frag_fb.o: \
+ /data/riotbuild/riotbase/sys/net/gnrc/network_layer/sixlowpan/frag/fb/gnrc_sixlowpan_frag_fb.c \
+ /data/riotbuild/riotproject/node/src/dev/lwm2m/bin/feather-nrf52840-sense/riotbuild/riotbuild.h \
+ /opt/gcc-arm-none-eabi-10.3-2021.10/lib/gcc/arm-none-eabi/10.3.1/include/stdint.h \
+ /opt/gcc-arm-none-eabi-10.3-2021.10/arm-none-eabi/include/stdint.h \
+ /opt/gcc-arm-none-eabi-10.3-2021.10/arm-none-eabi/include/machine/_default_types.h \
+ /opt/gcc-arm-none-eabi-10.3-2021.10/arm-none-eabi/include/sys/features.h \
+ /opt/gcc-arm-none-eabi-10.3-2021.10/arm-none-eabi/include/_newlib_version.h \
+ /opt/gcc-arm-none-eabi-10.3-2021.10/arm-none-eabi/include/sys/_intsup.h \
+ /opt/gcc-arm-none-eabi-10.3-2021.10/arm-none-eabi/include/sys/_stdint.h \
+ /opt/gcc-arm-none-eabi-10.3-2021.10/arm-none-eabi/include/string.h \
+ /opt/gcc-arm-none-eabi-10.3-2021.10/arm-none-eabi/include/_ansi.h \
+ /opt/gcc-arm-none-eabi-10.3-2021.10/arm-none-eabi/include/newlib-nano/newlib.h \
+ /opt/gcc-arm-none-eabi-10.3-2021.10/arm-none-eabi/include/sys/config.h \
+ /opt/gcc-arm-none-eabi-10.3-2021.10/arm-none-eabi/include/machine/ieeefp.h \
+ /opt/gcc-arm-none-eabi-10.3-2021.10/arm-none-eabi/include/sys/reent.h \
+ /opt/gcc-arm-none-eabi-10.3-2021.10/arm-none-eabi/include/_ansi.h \
+ /opt/gcc-arm-none-eabi-10.3-2021.10/lib/gcc/arm-none-eabi/10.3.1/include/stddef.h \
+ /opt/gcc-arm-none-eabi-10.3-2021.10/arm-none-eabi/include/sys/_types.h \
+ /opt/gcc-arm-none-eabi-10.3-2021.10/arm-none-eabi/include/machine/_types.h \
+ /opt/gcc-arm-none-eabi-10.3-2021.10/arm-none-eabi/include/sys/lock.h \
+ /data/riotbuild/riotbase/core/lib/include/assert.h \
+ /opt/gcc-arm-none-eabi-10.3-2021.10/arm-none-eabi/include/sys/cdefs.h \
+ /opt/gcc-arm-none-eabi-10.3-2021.10/arm-none-eabi/include/sys/string.h \
+ /data/riotbuild/riotbase/sys/include/net/gnrc/sixlowpan/config.h \
+ /data/riotbuild/riotbase/core/lib/include/modules.h \
+ /data/riotbuild/riotbase/sys/include/timex.h \
+ /opt/gcc-arm-none-eabi-10.3-2021.10/arm-none-eabi/include/inttypes.h \
+ /data/riotbuild/riotbase/sys/include/time_units.h \
+ /data/riotbuild/riotbase/sys/include/net/gnrc/sixlowpan/frag/fb.h \
+ /opt/gcc-arm-none-eabi-10.3-2021.10/lib/gcc/arm-none-eabi/10.3.1/include/stdbool.h \
+ /data/riotbuild/riotbase/core/include/msg.h \
+ /data/riotbuild/riotbase/core/include/sched.h \
+ /data/riotbuild/riotbase/core/lib/include/kernel_defines.h \
+ /data/riotbuild/riotbase/core/lib/include/modules.h \
+ /data/riotbuild/riotbase/core/lib/include/riot_version.h \
+ /data/riotbuild/riotbase/core/lib/include/compiler_hints.h \
+ /data/riotbuild/riotbase/core/lib/include/container.h \
+ /data/riotbuild/riotbase/core/include/native_sched.h \
+ /data/riotbuild/riotbase/core/lib/include/clist.h \
+ /data/riotbuild/riotbase/core/lib/include/list.h \
+ /data/riotbuild/riotbase/sys/include/net/gnrc/pkt.h \
+ /opt/gcc-arm-none-eabi-10.3-2021.10/arm-none-eabi/include/stdlib.h \
+ /opt/gcc-arm-none-eabi-10.3-2021.10/arm-none-eabi/include/machine/stdlib.h \
+ /data/riotbuild/riotbase/core/include/sched.h \
+ /data/riotbuild/riotbase/sys/include/net/gnrc/nettype.h \
+ /data/riotbuild/riotbase/sys/include/net/ethertype.h \
+ /data/riotbuild/riotbase/sys/include/net/protnum.h \
+ /data/riotbuild/riotbase/core/lib/include/list.h
+/data/riotbuild/riotproject/node/src/dev/lwm2m/bin/feather-nrf52840-sense/riotbuild/riotbuild.h:
+/opt/gcc-arm-none-eabi-10.3-2021.10/lib/gcc/arm-none-eabi/10.3.1/include/stdint.h:
+/opt/gcc-arm-none-eabi-10.3-2021.10/arm-none-eabi/include/stdint.h:
+/opt/gcc-arm-none-eabi-10.3-2021.10/arm-none-eabi/include/machine/_default_types.h:
+/opt/gcc-arm-none-eabi-10.3-2021.10/arm-none-eabi/include/sys/features.h:
+/opt/gcc-arm-none-eabi-10.3-2021.10/arm-none-eabi/include/_newlib_version.h:
+/opt/gcc-arm-none-eabi-10.3-2021.10/arm-none-eabi/include/sys/_intsup.h:
+/opt/gcc-arm-none-eabi-10.3-2021.10/arm-none-eabi/include/sys/_stdint.h:
+/opt/gcc-arm-none-eabi-10.3-2021.10/arm-none-eabi/include/string.h:
+/opt/gcc-arm-none-eabi-10.3-2021.10/arm-none-eabi/include/_ansi.h:
+/opt/gcc-arm-none-eabi-10.3-2021.10/arm-none-eabi/include/newlib-nano/newlib.h:
+/opt/gcc-arm-none-eabi-10.3-2021.10/arm-none-eabi/include/sys/config.h:
+/opt/gcc-arm-none-eabi-10.3-2021.10/arm-none-eabi/include/machine/ieeefp.h:
+/opt/gcc-arm-none-eabi-10.3-2021.10/arm-none-eabi/include/sys/reent.h:
+/opt/gcc-arm-none-eabi-10.3-2021.10/arm-none-eabi/include/_ansi.h:
+/opt/gcc-arm-none-eabi-10.3-2021.10/lib/gcc/arm-none-eabi/10.3.1/include/stddef.h:
+/opt/gcc-arm-none-eabi-10.3-2021.10/arm-none-eabi/include/sys/_types.h:
+/opt/gcc-arm-none-eabi-10.3-2021.10/arm-none-eabi/include/machine/_types.h:
+/opt/gcc-arm-none-eabi-10.3-2021.10/arm-none-eabi/include/sys/lock.h:
+/data/riotbuild/riotbase/core/lib/include/assert.h:
+/opt/gcc-arm-none-eabi-10.3-2021.10/arm-none-eabi/include/sys/cdefs.h:
+/opt/gcc-arm-none-eabi-10.3-2021.10/arm-none-eabi/include/sys/string.h:
+/data/riotbuild/riotbase/sys/include/net/gnrc/sixlowpan/config.h:
+/data/riotbuild/riotbase/core/lib/include/modules.h:
+/data/riotbuild/riotbase/sys/include/timex.h:
+/opt/gcc-arm-none-eabi-10.3-2021.10/arm-none-eabi/include/inttypes.h:
+/data/riotbuild/riotbase/sys/include/time_units.h:
+/data/riotbuild/riotbase/sys/include/net/gnrc/sixlowpan/frag/fb.h:
+/opt/gcc-arm-none-eabi-10.3-2021.10/lib/gcc/arm-none-eabi/10.3.1/include/stdbool.h:
+/data/riotbuild/riotbase/core/include/msg.h:
+/data/riotbuild/riotbase/core/include/sched.h:
+/data/riotbuild/riotbase/core/lib/include/kernel_defines.h:
+/data/riotbuild/riotbase/core/lib/include/modules.h:
+/data/riotbuild/riotbase/core/lib/include/riot_version.h:
+/data/riotbuild/riotbase/core/lib/include/compiler_hints.h:
+/data/riotbuild/riotbase/core/lib/include/container.h:
+/data/riotbuild/riotbase/core/include/native_sched.h:
+/data/riotbuild/riotbase/core/lib/include/clist.h:
+/data/riotbuild/riotbase/core/lib/include/list.h:
+/data/riotbuild/riotbase/sys/include/net/gnrc/pkt.h:
+/opt/gcc-arm-none-eabi-10.3-2021.10/arm-none-eabi/include/stdlib.h:
+/opt/gcc-arm-none-eabi-10.3-2021.10/arm-none-eabi/include/machine/stdlib.h:
+/data/riotbuild/riotbase/core/include/sched.h:
+/data/riotbuild/riotbase/sys/include/net/gnrc/nettype.h:
+/data/riotbuild/riotbase/sys/include/net/ethertype.h:
+/data/riotbuild/riotbase/sys/include/net/protnum.h:
+/data/riotbuild/riotbase/core/lib/include/list.h:
