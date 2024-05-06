2d0e38acc4f9bac353e2e780da55f94a56460695
annsann <git@annsann.eu>
2024-05-06 17:40:25 +0200

    Inform user that docs path changed

--- README.md
+++ README.md
@@ -6,7 +6,7 @@ Teamagochi is a synchronized always-online pet simulator with multiplayer functi
 
 It consists of a [physical device (Node)](./node/), [web backend](./web_backend/) and [web/mobile frontend](./web_backend/).
 
-### [Documentation Page](https://smartuni.github.io/teamagochi/)
+### [Documentation Page](https://smartuni.github.io/teamagochi/docs)
 
 ### [Kanban Board](https://github.com/orgs/smartuni/projects/2)
 
--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -9,6 +9,9 @@ function App() {
   return (
     <>
       <div>
+        <h1 className='font-extrabold text-9xl'>
+            Click <a href="./docs"> HERE </a> for Docs!
+        </h1>
         <a href="https://vitejs.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
@@ -16,14 +19,11 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React</h1>
+      <h1>Frontend Demo Page</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
       </div>
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
