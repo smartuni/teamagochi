1f0beb5235265929e1d5bb4fcfc123f6366e682c
annsann <git@annsann.eu>
2024-05-13 16:13:03 +0200

    Node: Shell Command send_event

--- node/code/inc/shell_commands.hpp
+++ node/code/inc/shell_commands.hpp
@@ -3,6 +3,8 @@
 #include "shell.h"
 #include <stdio.h>
 
+static kernel_pid_t DISPATCHER_THREAD_ID;
+
 static int echo_command(int argc, char **argv)
 {
     /* check that the command is called correctly */
@@ -18,8 +20,27 @@ static int echo_command(int argc, char **argv)
     return 0;
 }
 
+/**
+ * @brief Sends an event to the dispatcher.
+ * @example send_event 1
+*/
+static int send_event(int argc, char **argv) {
+    if (argc != 2) {
+        puts("usage: send_event <event>");
+        return 1;
+    }
+
+    msg_t message;
+    message.type = atoi(argv[1]);
+
+    msg_try_send(&message, DISPATCHER_THREAD_ID);
+
+    return 0;
+}
+
 const shell_command_t SHELL_COMMANDS[] = {
     { "echo", "Prints the message to the console", echo_command },
+    { "send_event", "Sends an event to the dispatcher. send_event 1", send_event },
     { NULL, NULL, NULL }
 };
 
