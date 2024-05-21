619f10f3345dd3e8f5d42a9b41b7a1a8fba47b29
rares <raresstefea9@gmail.com>
2024-05-21 17:28:48 +0200

    created footer

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,12 @@
 import { useState } from 'react'
 import './App.css'
 import LinkDevice from './Components/LinkDevice'
-
+import Footer from './Components/Footer';
 function App() {
- return <LinkDevice/>;
-
+ return [
+    <LinkDevice/>,
+    <Footer/>
+ ];
 }
 
 export default App
