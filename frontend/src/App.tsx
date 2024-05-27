86df4f533890f22570a5a9eeba5b3ce04479d6e8
Yousef-Taha1 <ytaha21@gmail.com>
2024-05-27 12:45:01 +0200

    Added auth feature

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,35 +1,59 @@
+import React from "react";
+import { useAuth } from "react-oidc-context";
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
+  const auth = useAuth();
   const [count, setCount] = useState(0)
 
-  return (
-    <>
+  switch (auth.activeNavigator) {
+    case 'signinSilent':
+      return <div>Signing you in...</div>;
+    case 'signoutRedirect':
+      return <div>Signing you out...</div>;
+  }
+  
+  if (auth.isLoading) {
+    return <div>Loading...</div>;
+  }
+
+  if (auth.error) {
+    return <div>Oops... {auth.error.message}</div>;
+  }
+
+  if (auth.isAuthenticated) {
+    return (
       <div>
-        <h1 className='font-extrabold text-9xl'>
-            Click <a href="./docs"> HERE </a> for Docs!
-        </h1>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <div>
+          <h1 className='font-extrabold text-9xl'>
+            Click <a href="./docs">HERE</a> for Docs!
+          </h1>
+          <a href="https://vitejs.dev" target="_blank">
+            <img src={viteLogo} className="logo" alt="Vite logo" />
+          </a>
+          <a href="https://react.dev" target="_blank">
+            <img src={reactLogo} className="logo react" alt="React logo" />
+          </a>
+        </div>
+        <h1>Frontend Demo Page</h1>
+        <div className="card">
+          <button onClick={() => setCount((count) => count + 1)}>
+            count is {count}
+          </button>
+          <button onClick={() => void auth.removeUser()}>Log out</button>
+        </div>
+        <p className="read-the-docs">
+          Click on the Vite and React logos to learn more
+        </p>
+        <div>Hello {auth.user?.profile.sub}</div>
       </div>
-      <h1>Frontend Demo Page</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    );
+  }
+
+  return <button onClick={() => void auth.signinRedirect()}>Log in</button>;
 }
 
-export default App
+export default App;
