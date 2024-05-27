a793ce6d6a9ba1dd437503838cdf05f70ff1821c
VanKHoiPham <van.pham@haw-hamburg.de>
2024-05-27 09:46:18 +0200

    backend: integrate keycloak identity and access management

---
+++ web_backend/src/main/java/authorization/UsersResource.java
@@ -0,0 +1,34 @@
+package authorization;
+
+import io.quarkus.security.identity.SecurityIdentity;
+import jakarta.inject.Inject;
+import jakarta.ws.rs.GET;
+import jakarta.ws.rs.Path;
+import org.jboss.resteasy.reactive.NoCache;
+
+@Path("/api/users")
+public class UsersResource {
+
+  @Inject
+  SecurityIdentity identity;
+
+  @GET
+  @Path("/me")
+  @NoCache
+  public User me() {
+    return new User(identity);
+  }
+
+  public static class User {
+
+    private final String userName;
+
+    User(SecurityIdentity identity) {
+      this.userName = identity.getPrincipal().getName();
+    }
+
+    public String getUserName() {
+      return userName;
+    }
+  }
+}
