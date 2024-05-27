4a99fd4370a2ecc83817b447a1a0e9bf803ec630
Yousef-Taha1 <ytaha21@gmail.com>
2024-05-27 16:24:57 +0200

    Fixed the auth merge

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -4,8 +4,6 @@ import { useState } from 'react'
 import './App.css'
 import LinkDevice from './Components/LinkDevice'
 import Footer from './Components/Footer';
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 
 function App() {
   const auth = useAuth();
@@ -34,22 +32,17 @@ function App() {
           <h1 className='font-extrabold text-9xl'>
             Click <a href="./docs">HERE</a> for Docs!
           </h1>
-          <a href="https://vitejs.dev" target="_blank">
-            <img src={viteLogo} className="logo" alt="Vite logo" />
-          </a>
-          <a href="https://react.dev" target="_blank">
-            <img src={reactLogo} className="logo react" alt="React logo" />
-          </a>
+
         </div>
         <h1>Frontend Demo Page</h1>
         <div className="card">
           <button onClick={() => setCount((count) => count + 1)}>
-            count is {count}
+            count is {count}[]
           </button>
           <button onClick={() => void auth.removeUser()}>Log out</button>
         </div>
         <p className="read-the-docs">
-          Click on the Vite and React logos to learn more
+          Hello
         </p>
         <div>Hello {auth.user?.profile.sub}</div>
       </div>
