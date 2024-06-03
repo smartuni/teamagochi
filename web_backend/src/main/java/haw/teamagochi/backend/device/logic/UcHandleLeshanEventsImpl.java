bb6a90ec3e827cc306b563e91dcd2620bc5fca50
ozfox <ozfox@posteo.de>
2024-06-03 23:33:50 +0200

    backend: add unfinished registration manager

--- web_backend/src/main/java/haw/teamagochi/backend/device/logic/UcHandleLeshanEventsImpl.java
+++ web_backend/src/main/java/haw/teamagochi/backend/device/logic/UcHandleLeshanEventsImpl.java
@@ -1,44 +1,57 @@
 package haw.teamagochi.backend.device.logic;
 
+import haw.teamagochi.backend.device.logic.registrationmanager.RegistrationManager;
 import haw.teamagochi.backend.leshanclient.datatypes.events.AwakeDto;
 import haw.teamagochi.backend.leshanclient.datatypes.events.CoaplogDto;
 import haw.teamagochi.backend.leshanclient.datatypes.events.RegistrationDto;
 import haw.teamagochi.backend.leshanclient.datatypes.events.UpdatedDto;
 import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.inject.Inject;
+import org.jboss.logging.Logger;
 
-/**
- * Default implementation of {@link UcHandleLeshanEvents}.
- */
+/** Default implementation of {@link UcHandleLeshanEvents}. */
 @ApplicationScoped
 public class UcHandleLeshanEventsImpl implements UcHandleLeshanEvents {
 
+  private static final Logger LOGGER = Logger.getLogger(UcHandleLeshanEventsImpl.class);
+
+  @Inject RegistrationManager registrationManager;
+
   @Override
   public void handleRegistration(RegistrationDto dto) {
-    System.out.println("Registration: " + dto.registrationId);
+    LOGGER.debug("Received registration event: " + dto.endpoint + " (" + dto.registrationId + ")");
+
+    registrationManager.addClient(dto.endpoint);
   }
 
   @Override
   public void handleDeregistration(RegistrationDto dto) {
-    System.out.println("Deregistration: " + dto.registrationId);
+    LOGGER.debug(
+        "Received deregistration event: " + dto.endpoint + " (" + dto.registrationId + ")");
   }
 
   @Override
   public void handleUpdate(UpdatedDto dto) {
-    System.out.println("Updated: " + dto.update.registrationId);
+    LOGGER.debug(
+        "Received update event: "
+            + dto.registration.endpoint
+            + "("
+            + dto.update.registrationId
+            + ")");
   }
 
   @Override
   public void handleSleeping(AwakeDto dto) {
-    System.out.println("Sleeping: " + dto.ep);
+    LOGGER.debug("Received sleeping event: " + dto.ep);
   }
 
   @Override
   public void handleAwake(AwakeDto dto) {
-    System.out.println("Awake: " + dto.ep);
+    LOGGER.debug("Received awake event: " + dto.ep);
   }
 
   @Override
   public void handleCoapLog(CoaplogDto dto) {
-    System.out.println("CoapLog: " + dto.ep);
+    LOGGER.debug("Received coap logging event: " + dto.ep);
   }
 }
