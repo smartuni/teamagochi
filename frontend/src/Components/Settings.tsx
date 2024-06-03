72b2ef6c866ff90c7f99692731d2edc235486890
Your Name <raresstefea9@gmail.com>
2024-06-03 16:28:11 +0200

    profile card created

--- frontend/src/Components/Settings.tsx
+++ frontend/src/Components/Settings.tsx
@@ -4,13 +4,22 @@ function Settings(){
 return (
    
     <div className='d-flex justify-content-center pt-4'>
-    <div className="card align-items-center">
+    <div className="card align-items-center text-bg-light">
         <img src={profile_pic1} className="card-img-top py-3" alt="profile picture" style={{ width: "200px", height: "200px" }} />
-        <h5 className="card-title text-center">Profile</h5>
-        <div className="card-body">
-            This is some text within a card body.
-        </div>
-    </div>
+            <h5 className="card-title text-center"><kbd className='bg-success text-white'>Profile</kbd></h5>
+                <div className="card-body">                     
+                    <div className="input-group mb-2 mr-sm-2">
+                        <div className="input-group-prepend">
+                            <div className="input-group-text">@</div>
+                        </div>
+                        <input type="text" className="form-control" placeholder="Pedro" disabled></input>
+                    </div>
+                    <div className='d-flex justify-content-center pt-2'>
+                        <button type="button" className="btn btn-outline-danger btn-block w-100" >Logout</button>
+                    </div>
+                </div>
+            
+            </div>
     </div>
 
 
