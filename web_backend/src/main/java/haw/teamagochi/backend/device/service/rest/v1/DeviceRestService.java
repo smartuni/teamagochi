ed8dae572ffb2f798ee6af7d725bd67edf678f68
ozfox <ozfox@posteo.de>
2024-06-04 12:51:07 +0200

    backend: add OpenAPI spec generator and update DeviceRestService

--- web_backend/src/main/java/haw/teamagochi/backend/device/service/rest/v1/DeviceRestService.java
+++ web_backend/src/main/java/haw/teamagochi/backend/device/service/rest/v1/DeviceRestService.java
@@ -1,56 +1,83 @@
 package haw.teamagochi.backend.device.service.rest.v1;
-import haw.teamagochi.backend.device.service.rest.v1.model.DeviceDTO;
+
 import haw.teamagochi.backend.device.service.rest.v1.mapper.DeviceMapper;
-import haw.teamagochi.backend.app.Services.RegistrationManager;
+import haw.teamagochi.backend.device.service.rest.v1.model.DeviceDTO;
 import jakarta.inject.Inject;
 import jakarta.ws.rs.GET;
-import jakarta.ws.rs.HeaderParam;
+import jakarta.ws.rs.NotFoundException;
 import jakarta.ws.rs.POST;
 import jakarta.ws.rs.Path;
+import jakarta.ws.rs.PathParam;
+import java.util.ArrayList;
 import java.util.List;
+import org.eclipse.microprofile.openapi.annotations.Operation;
+import org.eclipse.microprofile.openapi.annotations.responses.APIResponse;
+import org.eclipse.microprofile.openapi.annotations.responses.APIResponses;
+import org.eclipse.microprofile.openapi.annotations.tags.Tag;
 
-//@RegisterRestClient //needed for oidc??
-@Path("Device")
+/**
+ * Rest interface for the device component.
+ */
+@Path("/v1/device")
+@Tag(name = "device", description = "Everything about devices.")
+@APIResponses(value = {
+
+})
 public class DeviceRestService {
-//Map Struct --> Mapper --> entitys auf json
+
   @Inject
-  DeviceMapper deviceMapper;
-
-
-  /**
-   * @param userAuthToken --> the user to get the devices for
-   * @return List of Devices from the given user --> toBeDefined --> what info is needed: Name,ID....
-   */
-  @GET
-  public List<DeviceDTO> getDevices(@HeaderParam("Authorization") String userAuthToken){ //ID + Name
-    //TODO -->
-    return null;
-  }
+  protected DeviceMapper deviceMapper;
 
   /**
-   * Is this even needed?
-   * @param deviceID --> the device to get further Information for
-   * @param userAuthToken --> the user
-   * @return Device information --> toBeDefined --> what info is needed: Name,ID, Current Pet,....
-   */
-  @Path("{deviceID}")
-  @GET
-  public DeviceDTO getDevice(String deviceID, @HeaderParam("Authorization") String userAuthToken){ //ID + Name + Pet ....
-    //TODO --> check if user is authorized, get Device from DB, create DeviceDTO
-    return null;
-  }
-
-  /**
+   * Get all devices.
    *
-   * @param registerCode the code displyed on the device
-   * @param userAuthToken --> the user to get the devices for
-   * @return the device Information if succesfull, else null
+   * @return a list of all {@link DeviceDTO DeviceDTOs}, possibly empty
    */
-  @Path("register/{registerCode}")
-  @POST
-  public DeviceDTO registerDevice(String registerCode, @HeaderParam("Authorization") String userAuthToken){ //--> name missing
-  return null;
+  @GET
+  @Operation(summary = "Get all devices")
+  @APIResponse(responseCode = "200")
+  public List<DeviceDTO> getDevices() {
+    // TODO replace with real implementation
+    return new ArrayList<>();
   }
 
-  //TODO --> Device give name;
+  /**
+   * Get a device by its id.
+   *
+   * @param deviceId of the device to get
+   * @return the {@link DeviceDTO} if found
+   * @throws NotFoundException if no device was found
+   */
+  @GET
+  @Path("/{deviceId}")
+  @Operation(summary = "Get a device by its id")
+  @APIResponse(responseCode = "200")
+  @APIResponse(responseCode = "404", description = "Not Found")
+  public DeviceDTO getDeviceById(@PathParam("deviceId") long deviceId) {
+    // TODO replace with real implementation
+    if (deviceId == 1) {
+      return new DeviceDTO(deviceId, "mock-device");
+    }
+    throw new NotFoundException();
+  }
+
+  /**
+   * Register a device using a registration code.
+   *
+   * @param registrationCode the registration code for a device
+   * @return the {@link DeviceDTO} if registration code is valid
+   * @throws NotFoundException if the registration code is not found
+   */
+  @POST
+  @Path("/register/{registrationCode}")
+  @Operation(summary = "Register a device using a registration code")
+  @APIResponse(responseCode = "200")
+  @APIResponse(responseCode = "404", description = "Not Found")
+  public DeviceDTO registerDevice(@PathParam("registrationCode") String registrationCode) {
+    // TODO replace with real implementation
+    if (registrationCode.equals("aaaaaa")) {
+      return new DeviceDTO(1, "mock-device");
+    }
+    throw new NotFoundException();
+  }
 }
---
+++ web_backend/src/test/java/haw/teamagochi/backend/device/service/rest/v1/DeviceRestServiceTests.java
@@ -0,0 +1,49 @@
+package haw.teamagochi.backend.device.service.rest.v1;
+
+import static io.restassured.RestAssured.when;
+import static org.hamcrest.CoreMatchers.is;
+
+import haw.teamagochi.backend.device.service.rest.v1.model.DeviceDTO;
+import io.quarkus.test.common.http.TestHTTPEndpoint;
+import io.quarkus.test.junit.QuarkusTest;
+import org.junit.jupiter.api.Test;
+
+/**
+ * Tests for {@link DeviceRestService}.
+ */
+@QuarkusTest
+@TestHTTPEndpoint(DeviceRestService.class)
+public class DeviceRestServiceTests {
+
+  // TODO implement the real test
+  @Test
+  public void testGetAllDevices() {
+    when().get("/")
+        .then()
+        .statusCode(200)
+        .body(is("[]"));
+  }
+
+  // TODO implement the real test
+  @Test
+  public void testGetDeviceById() {
+    DeviceDTO result =
+        when().get("/1")
+          .then()
+          .statusCode(200)
+          .extract()
+          .as(DeviceDTO.class);
+    System.out.println(result.getDeviceID());
+    System.out.println(result.getDeviceName());
+  }
+
+  // TODO implement the real test
+  @Test
+  public void testGetDeviceById_NotFound() {
+    when()
+        .get("/99")
+        .then()
+        .statusCode(404)
+        .body(is(""));
+  }
+}
