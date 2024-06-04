3fffb948a0695623f8ea8784e72f4d99ae055747
Moritz <moritz.holzer@haw-hamburg.de>
2024-06-04 10:53:08 +0200

    moved fsm in own  module

--- node/code/modules/fsm/include/fsm.h
+++ node/code/modules/fsm/include/fsm.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (C) 
+ * Copyright (C) 2024 HAW-Hamburg
  *
  * This file is subject to the terms and conditions of the GNU Lesser
  * General Public License v2.1. See the file LICENSE in the top level
