0788bde77f94bdf59f816e711a95a7a931a8ec09
Samuel Costa <samcosta333@gmail.com>
2024-06-17 17:18:08 +0200

    Added CreatePetModal with pet creation logistics

--- frontend/src/Components/CreatePetModal.tsx
+++ frontend/src/Components/CreatePetModal.tsx
@@ -1,16 +1,18 @@
 import { SetStateAction, useState } from 'react';
 import Popup from 'reactjs-popup';
 import 'reactjs-popup/dist/index.css';
-import profile_pic1 from '../Misc/8-bit-dog-nobg.png';
+import profile_land from '../Misc/8-bit-dog-nobg.png';
+import profile_air from '../Misc/duck_8bit.png';
+import profile_water from '../Misc/fish_8bit.png'
 
 const CreatePetModal = () => {
     const [open, setOpen] = useState(false);
     const [petName, setPetName] = useState("");
-    const [petType, setPetType] = useState("");
+    const [petType, setPetType] = useState("Land");
 
     const handleClose = () => {
         setPetName("");
-        setPetType("");
+        setPetType("Land");
         setOpen(false);
     };
 
@@ -35,15 +37,15 @@ const CreatePetModal = () => {
   return (
         <>
             <div className='container-fluid d-flex justify-content-center align-items-center' style={{ height: '100vh' }}>
-                <button className="btn btn-success" onClick={() => setOpen(true)}>Create Tomogachi</button>
+                <button className="btn btn-success" onClick={() => setOpen(true)}>Create Teamagochi</button>
             </div>
             <Popup
                 open={open}
                 onClose={handleClose}
                 overlayStyle={{ background: 'rgba(0, 0, 0, 0.5)' }}
-                contentStyle={{ borderRadius: '10px', border: '2px solid grey', width: 'fit-content' }}
+                contentStyle={{ borderRadius: '10px', border: '2px solid grey', width: '50%' }}
             > 
-                <div style={{ padding: '20px', position: 'relative' }}>
+                <div style={{padding: '20px', position: 'relative' }}>
                 <button
                         type="button"
                         className="btn-close"
@@ -60,8 +62,20 @@ const CreatePetModal = () => {
                         </figure>
 
                         {petType === "Land" &&
-                            <div className="card align-items-center m-3" style={{ width: "30%" }}>
-                                <img src={profile_pic1} className="card-img-top py-3" alt="profile picture" style={{ width: "100%", height: "auto" }} />
+                            <div className="card align-items-center m-3" style={{ width: "40%" }}>
+                                <img src={profile_land} className="card-img-top py-3" style={{ width: "80%", height: "auto" }} />
+                            </div>
+                        }
+
+                        {petType == "Air" &&
+                            <div className="card align-items-center m-3" style={{ width: "40%" }}>
+                                <img src={profile_air} className="card-img-top py-3" alt="profile picture" style={{ width: "80%", height: "auto" }} />
+                            </div>
+                        }
+
+                        {petType == "Water" &&
+                            <div className="card align-items-center m-3" style={{ width: "40%" }}>
+                                <img src={profile_water} className="card-img-top py-3" alt="profile picture" style={{ width: "80%", height: "auto" }} />
                             </div>
                         }
 
@@ -69,36 +83,22 @@ const CreatePetModal = () => {
                             <span className="input-group-text">@</span>
                             <label htmlFor="inputLink" className="visually-hidden">Pet Name</label>
                             <input type="text" className="form-control" id="inputPetName" 
-                            placeholder="Pet Name" required value={petName} onChange={handleInputPetName}/>
+                                placeholder="Pet Name" required value={petName} onChange={handleInputPetName}/>
                             <div className='invalid-feedback'>ok</div>
                         </div>
-                        
-                        {/*<div className="container-fluid d-flex justify-content-center">
-                            <button className="btn btn-dark px-5 m-2" onClick={() => setPetType("Land")}>Land</button>
-                            <button className="btn btn-secondary px-5 m-2" onClick={() => setPetType("Air")}>Air</button>
-                            <button className="btn btn-primary px-5 m-2" onClick={() => setPetType("Water")}>Water</button>
-                        </div>*/}
 
-                        {/*<div className="btn-group btn-group-toggle" data-toggle="buttons">
-                           <label className="btn btn-dark active">
-                                <input type="radio" name="Land" id="Land" checked>Land</input>
-                            </label>
-                            <label className="btn btn-secondary">
-                                <input type="radio" name="Air" id="Air">Air</input>
-                            </label>
-                            <label className="btn btn-primary">
-                                <input type="radio" name="Water" id="Water">Water</input>
-                            </label>
-                        </div> */}
+                        <div className= "btn-group btn-group-toggle pt-2" data-toggle="buttons">
+                            <input type="radio" className="btn-check" name="options" id="option1" value="Land" autoComplete="off" 
+                                checked={petType === "Land"} onChange={handleInputPetType}></input>
+                            <label className="btn btn-secondary" htmlFor="option1">Land</label>
 
-                        <div className="select-group mb-1 py-2">
-                            <select className="form-select" aria-label="Choose Pet Type" onChange={handleInputPetType}>
-                                <option value="">Choose Pet Type</option>
-                                <option value="Land">Land</option>
-                                <option value="Air">Air</option>
-                                <option value="Water">Water</option>
-                            </select>
-                            <div className="invalid-feedback">ok</div>
+                            <input type="radio" className="btn-check" name="options" id="option2" value="Air" autoComplete="off" 
+                                checked={petType === "Air"} onChange={handleInputPetType}></input>
+                            <label className="btn btn-secondary" htmlFor="option2">Air</label>
+
+                            <input type="radio" className="btn-check" name="options" id="option3" value="Water" autoComplete="off" 
+                                checked={petType === "Water"} onChange={handleInputPetType}></input>
+                            <label className="btn btn-secondary" htmlFor='option3'>Water</label>
                         </div>
 
                         <button type='submit' className='btn btn-success mt-2 mb-2'>
