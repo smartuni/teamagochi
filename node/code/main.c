d1d2242807450282bf82e35c19c2c2d0f98ff8c0
e-dddos <eduard.lomtadze@gmail.com>
2024-06-17 15:07:03 +0200

    revert main

--- node/code/main.c
+++ node/code/main.c
@@ -4,21 +4,14 @@
 #include "events.h"
 #include "fsm.h"
 #include "thread.h"
-#include "io_handler.h"
-#include "init_buttons.h"
-
 
 #define SHELL_QUEUE_SIZE (8)
 static msg_t _shell_queue[SHELL_QUEUE_SIZE];
 
 
 int main(void)
-{   
+{    
     fsm_start_thread();
     msg_init_queue(_shell_queue, SHELL_QUEUE_SIZE);
-    while (1) {
-        vibrate(500);
-        ztimer_sleep(ZTIMER_MSEC, 500);
-    }
     shell_loop();
 }
