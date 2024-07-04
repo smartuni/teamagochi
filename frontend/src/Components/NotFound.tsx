4d0016a73996f48d1d9005a57b8ee196fefa5cbe
ozfox <ozfox@posteo.de>
2024-07-04 22:15:46 +0200

    frontend: router and layout

---
+++ frontend/src/Components/NotFound.tsx
@@ -0,0 +1,16 @@
+const NotFound = () => {
+   return (
+    <div className="container-lg h-100">
+      <div className="d-flex flex-column align-items-center p-4">
+        <div className="mt-5 pt-5">
+          <h1>404</h1>
+        </div>
+        <div>
+          <h4>Not found</h4>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default NotFound;
